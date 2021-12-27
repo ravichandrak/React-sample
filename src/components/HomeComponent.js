cacb5fa0ab86d4d3a742f536e581b476ba53e8f9
Ravichandra K <Ravichandra.K@Ltts.com>
2021-12-27 17:53:58 +0530

    Fetch Handling Errors

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle } from 'reactstrap';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 function RenderCard({item, isLoading, errMess}) {
     if (isLoading) {
@@ -16,7 +17,7 @@ function RenderCard({item, isLoading, errMess}) {
     else 
         return(
             <Card>
-                <CardImg width="100%" src={item?.image} alt={item?.description}></CardImg>
+                <CardImg src={baseUrl + item?.image} alt={item?.name} />
                 <CardBody>
                     <CardTitle>{item?.name}</CardTitle>
                     { 
@@ -36,10 +37,10 @@ function Home(props) {
                     <RenderCard item={props.dish} isLoading={props.dishesLoading} errMess={props.dishesErrMess}  />
                 </div>
                 <div className="col-12 col-md m-1">
-                    <RenderCard item={props.dish} isLoading={props.dishesLoading} errMess={props.dishesErrMess}  />
+                    <RenderCard item={props.promotion} isLoading={props.promoLoading} errMess={props.promoErrMess} />
                 </div>
                 <div className="col-12 col-md m-1">
-                    <RenderCard item={props.dish} isLoading={props.dishesLoading} errMess={props.dishesErrMess}  />
+                    <RenderCard item={props.leader}/>
                 </div>
             </div>
         </div>
